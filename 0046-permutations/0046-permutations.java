31022a8481d528e602eda77a31f057c8660966a3
srikanthdwa0925-crypto <srikanthdwa0925@gmail.com>
2026-09-15 19:13:41 +0530

    Time: 1 ms (87.97%), Space: 45.4 MB (78.49%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void recuPermut( int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean fre[]){
+        if(ds.size() == nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+
+        }
+        for(int i=0; i<nums.length; i++){
+            if(!fre[i]){
+                fre[i]=true;
+                ds.add(nums[i]);
+                recuPermut(nums,ds,ans,fre);
+                ds.remove(ds.size()-1);
+                fre[i]=false;  
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        boolean fre[]=new boolean[nums.length];
+        recuPermut(nums,ds,ans,fre);
+        return ans;
+        
+        
+    }
+}
