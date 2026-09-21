dcadabac353fb25035c1fabd694038d5c5a0d2b1
srikanthdwa0925-crypto <srikanthdwa0925@gmail.com>
2026-09-21 20:24:53 +0530

    Time: 1600 ms (8.79%), Space: 46.1 MB (42.88%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s,int left,int right){
+        while(left<right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return true;
+    }
+    public String longestPalindrome(String s) {
+        String result="";
+        for(int i=0; i<s.length(); i++){
+            for(int j=i; j<s.length(); j++){
+                if(isPalindrome(s,i,j)){
+                    if((j-i+1) > result.length()){
+                        result=s.substring(i,j+1);
+
+
+                    }
+                }
+
+            }
+        }
+        return result;
+        
+    }
+}
